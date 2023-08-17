5ec8f6cab3ced744958437fce688416fa5e7e6d2
EzequielDiaz777 <diazezequiel777@gmail.com>
2023-08-16 22:44:28 -0300

    primer commit

---
+++ src/practicagh3/PracticaGH3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicagh3;
+
+/**
+ *
+ * @author long_
+ */
+public class PracticaGH3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Linea escrita desde NB");
+    }
+    
+}
